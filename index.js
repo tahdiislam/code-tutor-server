9a27d6bba52419d98dce44051677804ab529c6c6
tahdi-islam <tshdi60@gmail.com>
2022-10-27 06:06:34 +0600

    run the server

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = 
+const port = process.env.port || 5000;
 
 app.use(cors())
+
+app.get("/", (req, res) => {
+    res.send("code tutor server is running")
+})
+
+app.listen(port, () => {
+    console.log(`Code Tutor server running on port ${port}`);
+})
